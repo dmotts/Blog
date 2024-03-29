eb3ec071a65343d8534dc6425f55c1490ab62bed
Daley Mottley <daley.mottley@hotmail.com>
2024-03-29 03:44:55 +0000

    Home page loaded

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Define a view for the home page
+def home_view(request):
+    return HttpResponse('<h1>Welcome to My Blog</h1>')
